a88793d26edebc25efeade45f5f8f9212b8e3fa1
Joshua Shi <thehandsomepanther@gmail.com>
2015-10-03 17:27:42 -0500

    prep for launch

--- modules/users/client/config/users.client.routes.js
+++ modules/users/client/config/users.client.routes.js
@@ -18,10 +18,10 @@ angular.module('users').config(['$stateProvider',
 				url: '/password',
 				templateUrl: 'modules/users/views/settings/change-password.client.view.html'
 			}).
-			state('settings.accounts', {
-				url: '/accounts',
-				templateUrl: 'modules/users/views/settings/manage-social-accounts.client.view.html'
-			}).
+			// state('settings.accounts', {
+			// 	url: '/accounts',
+			// 	templateUrl: 'modules/users/views/settings/manage-social-accounts.client.view.html'
+			// }).
 			state('settings.picture', {
 				url: '/picture',
 				templateUrl: 'modules/users/views/settings/change-profile-picture.client.view.html'
